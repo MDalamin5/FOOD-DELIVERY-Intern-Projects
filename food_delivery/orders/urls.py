05010fa645eccf9a1a7332da7c87de5d4892a5d3
MDalamin5 <mdal.amin5@northsouth.edu>
2024-05-27 22:40:27 +0600

    finally done Intern Projects

--- food_delivery/orders/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('toggle_meal/<int:subscription_id>/<str:meal_type>/', views.toggle_meal, name='toggle_meal'),
-    path('subscription/<int:subscription_id>/', views.subscription_detail, name='subscription_detail'),
-    path('order-counts-daily/', views.order_counts_daily, name='order_counts_daily'),
-]
-
