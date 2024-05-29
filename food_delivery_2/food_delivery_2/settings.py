fe25a398e5f70664cc23de572a7127b08d6407c6
MDalamin5 <mdal.amin5@northsouth.edu>
2024-05-29 19:22:37 +0600

    allhamdulilla done

--- food_delivery_2/food_delivery_2/settings.py
+++ food_delivery_2/food_delivery_2/settings.py
@@ -136,9 +136,9 @@ import os
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+# ]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
